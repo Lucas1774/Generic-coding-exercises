49db91946ceff471123353b25b3262dff7a28913
L <L>
2023-05-14 00:00:50 +0200

    Clean

--- src/IPAddressDetector.cpp
+++ src/IPAddressDetector.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 #include <string>
 #include <regex>
-#include <list>
 
 using namespace std;
 
 bool isValid(string s) {
-    regex valid_address("([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}");
-    bool temp = regex_match(s, valid_address);
-    return temp;
+    const regex VALID_ADDRESS("([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}");
+    return regex_match(s, VALID_ADDRESS);
 }
 
 
 int main (int argc, char* argv[]) {
-    if (argc !=2) {
+    if (argc != 2) {
         cout << "program call doesn't match expected format .\\IPAddressDetector.exe <string>" << endl;
         return 1; 
     }
     string input = argv[1];
     if (input.size() < 7) {
-        return 1;
+        return 0;
     }
-    for (int i = 0; i < input.size(); ++i) {
-        for (int j = 7 ; j < 15 && i + j <= input.length(); ++j) {
-            string substr = input.substr(i, j);
+    string substr;
+    for (int i = 0; i < input.size(); i++) {
+        for (int j = 7 ; j < 15 && i + j <= input.length(); j++) {
+            substr = input.substr(i, j);
             if ( isValid(substr)) {
                 cout << substr << endl;
                 i += j - 1;
